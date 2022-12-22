7dde9beab49a8a80e0d3409279b35979d61761d3
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-12-22 13:26:48 +0200

    Add project files.

---
+++ Lab_0.8.2/Lab_0.8.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string Change(string& s)
+{
+	int i = 0;
+	int len = s.length();
+	size_t pos = 0;
+
+	while ((pos = s.find('.', pos)) < (len / 1) * 1 - 1)
+	{
+		s[pos+1] = toupper(s[pos+1]);
+		pos++;
+	}
+	return s;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	string dest = Change(str);
+	
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_0.8.2/Lab_0.8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str{ "hi...Kl" };
+			string t = Change(str);
+			string l{ "hi...Kl" };
+			Assert::AreEqual(t, l);
+		}
+	};
+}
